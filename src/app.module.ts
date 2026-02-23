cb9e933bd415eca81389489a51fe18b965a9c63d
Siyam Talukder <mdsiyamtalukder1@gmail.com>
2026-02-23 06:15:19 +0600

    app related

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CustomersModule } from './customers/customers.module';
+import { CustomersController } from './customers/customers.controller';
+import { CustomersService } from './customers/customers.service';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CustomersModule ],
+  controllers: [AppController, CustomersController],
+  providers: [AppService,CustomersService],
 })
 export class AppModule {}
