1af9f37840069e461bb2e6b4bea9f1de30e9657c
Siyam Talukder <mdsiyamtalukder1@gmail.com>
2026-04-06 06:59:03 +0600

    last viva

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Post, Body, HttpCode, HttpStatus, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, HttpStatus, UsePipes, ValidationPipe, Res } from '@nestjs/common';
+import express from 'express'; // <-- 1. Import this from express
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/login.dto';
 
@@ -9,7 +10,19 @@ export class AuthController {
   @HttpCode(HttpStatus.OK) 
   @Post('login')
   @UsePipes(new ValidationPipe())
-  login(@Body() loginDto: LoginDto) {
-    return this.authService.login(loginDto);
+  async login(
+    @Body() loginDto: LoginDto, 
+    @Res({ passthrough: true }) res: express.Response 
+  ) {
+    const result = await this.authService.login(loginDto);
+    res.cookie('access_token', result.access_token, {
+      httpOnly: true, 
+      sameSite: 'none',
+      secure: false, 
+      path: '/',
+      maxAge: 300 * 60 * 1000, 
+    });
+
+    return { message: "Login successful" };
   }
 }
